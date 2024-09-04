0703d3557ac2327b4d5fc2e6fe1683a4df5f659d
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-09-04 14:30:53 +0900

    [Silver IV] Title: 큐, Time: 136 ms, Memory: 112016 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,30 +1,31 @@
 import sys
 
-res = []
-n = int(input())
+res=[]
+n=int(input())
 
 for i in range(n):
     data = list(sys.stdin.readline().split())
-    if data[0] == "push":
+    if data[0]=="push":
         res.append(data[1])
-    if data[0] == "pop":
+    if data[0]=="pop":
         if res:
             print(res.pop(0))
         else:
             print(-1)
-    if data[0] == "size":
+    if data[0]=="size":
         print(len(res))
-    if data[0] == "empty":
+
+    if data[0]=="empty":
         if res:
             print(0)
         else:
             print(1)
-    if data[0] == "front":
+    if data[0]=="front":
         if res:
             print(res[0])
         else:
             print(-1)
-    if data[0] == "back":
+    if data[0]=="back":
         if res:
             print(res[-1])
         else:
