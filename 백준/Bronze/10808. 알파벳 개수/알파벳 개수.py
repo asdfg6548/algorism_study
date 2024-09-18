657539d96c2aa170e37f0d241fcb6aebf409d065
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-09-18 15:57:49 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,9 +1,15 @@
 import sys
-
 input = sys.stdin.readline
-al_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-           't', 'u', 'v', 'w', 'x', 'y', 'z']
-word = list(input())
 
-for i in range(len(al_list)):
-    print(word.count(al_list[i]),end=" ")
+al_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+word = input().rstrip()  # 줄바꿈 문자 제거
+
+# 빈도수 계산
+frequency = {char: 0 for char in al_list}
+for char in word:
+    if char in frequency:
+        frequency[char] += 1
+
+# 결과 출력
+for char in al_list:
+    print(frequency[char], end=" ")
