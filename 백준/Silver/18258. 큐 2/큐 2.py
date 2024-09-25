1f39960193796a13f3e8e28b0d8b2c840f386e93
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-09-25 15:19:57 +0900

    [Silver IV] Title: 큐 2, Time: 800 ms, Memory: 253388 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n=int(input())
+
+res=deque()
+
+for i in range(n):
+    inp=list(input().split())
+    if inp[0]=="push":
+        res.append(inp[1])
+    elif inp[0]=="pop":
+        if res:
+            print(res.popleft())
+        else:
+            print(-1)
+    elif inp[0]=="size":
+        print(len(res))
+    elif inp[0]=="empty":
+        if res:
+            print(0)
+        else:
+            print(1)
+    elif inp[0]=="front":
+        if res:
+            print(res[0])
+        else:
+            print(-1)
+    elif inp[0]=="back":
+        if res:
+            print(res[-1])
+        else:
+            print(-1)
+
