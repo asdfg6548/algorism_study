26926e13f185fb4da7f8a6a81700951bd1d5f04f
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-09-25 16:08:27 +0900

    [Silver IV] Title: 덱, Time: 152 ms, Memory: 115376 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,46 +1,40 @@
 import sys
+input = sys.stdin.readline
 from collections import deque
 
-res=deque()
 n=int(input())
 
-for i in range(n):
-    data = list(sys.stdin.readline().split())
-    if data[0]=="push_front":
-        res.appendleft(data[1])
+res=deque()
 
-    if data[0] == "push_back":
-        res.append(data[1])
-    
-    if data[0]=="pop_front":
+for i in range(n):
+    inp=list(input().split())
+    if inp[0]=="push_front":
+        res.appendleft(inp[1])
+    elif inp[0]=="push_back":
+        res.append(inp[1])
+    elif inp[0]=="pop_front":
         if res:
             print(res.popleft())
         else:
             print(-1)
-
-    if data[0]=="pop_back":
+    elif inp[0]=="pop_back":
         if res:
             print(res.pop())
         else:
             print(-1)
-
-            
-    if data[0]=="size":
+    elif inp[0]=="size":
         print(len(res))
-        
-    if data[0]=="empty":
+    elif inp[0]=="empty":
         if res:
             print(0)
         else:
             print(1)
-            
-    if data[0]=="front":
+    elif inp[0]=="front":
         if res:
             print(res[0])
         else:
             print(-1)
-            
-    if data[0]=="back":
+    elif inp[0]=="back":
         if res:
             print(res[-1])
         else:
