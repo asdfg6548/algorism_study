0069cd73163685b8cba0020c93a458b10729c5c6
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-10-04 14:41:12 +0900

    [Gold IV] Title: 오큰수, Time: 464 ms, Memory: 248148 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,14 +1,15 @@
 import sys
 input=sys.stdin.readline
 
-N = int(input())  # 입력 받기
-lis = list(map(int, input().split()))  # 수열을 리스트로 변환
-result = [-1] * N  # 결과를 저장할 리스트 (기본값은 -1)
-stack = []  # 인덱스를 저장할 스택
+n=int(input())
+data=list(map(int,input().split()))
 
-for i in range(N):
-    while stack and lis[i] > lis[stack[-1]]:  # 스택이 비어 있지 않고, 현재 원소가 스택의 맨 위 원소보다 클 때
-        result[stack[-1]] = lis[i]  # 스택의 맨 위 인덱스에 해당하는 원소의 오큰수를 현재 원소로 설정
-        stack.pop()  # 스택에서 맨 위 원소를 제거
-    stack.append(i)  # 현재 인덱스를 스택에 추가
-print(*result)  # 결과 출력
+res=[-1]*n
+stack=[]
+
+for i in range(n):
+    while stack and data[i]>data[stack[-1]]:
+        res[stack[-1]]=data[i]
+        stack.pop()
+    stack.append(i)
+print(*res)
