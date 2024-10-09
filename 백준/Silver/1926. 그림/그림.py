e35279d52e1f7f22e3edc2f6d9993f9418ff9008
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-10-09 15:49:29 +0900

    [Silver I] Title: 그림, Time: 160 ms, Memory: 122636 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,56 +1,49 @@
-"""
-1. 아이디어
-- 2중 for => 값 1 && 방문X => BFS
-- BFS 돌면서 그림 개수 +1, 최대값을 갱신
-
-2. 시간복잡도
-- BFS : O(V+E)
-- V : 500 * 500
-- E : 4 * 500 * 500
-- V+E : 5 * 250000 = 100만 < 2억 >> 가능!
-
-3. 자료구조
-- 그래프 전체 지도 : int[][]
-- 방문 : bool[][]
-- Queue(BFS)
-"""
-
 from collections import deque
-
 import sys
+
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(n)]
-chk = [[False] * m for _ in range(n)]
+n, m = map(int, input().split())
+graph = []
+cnt=0
+res=[]
 
-dy = [0,1,0,-1]
-dx = [1,0,-1,0]
+for i in range(n):
+    graph.append(list(map(int, input().rstrip().split())))
 
-def bfs(y, x):
-    rs = 1
-    q = deque()
-    q.append((y, x))
-    while q:
-        ey, ex = q.popleft()
-        for k in range(4):
-            ny = ey + dy[k]
-            nx = ex + dx[k]
-            if 0<=ny<n and 0<=nx<m:
-                if map[ny][nx] == 1 and chk[ny][nx] == False:
-                    rs += 1
-                    chk[ny][nx] = True
-                    q.append((ny,nx))
-    return rs
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-cnt = 0
-maxv = 0
-for j in range(n):
-    for i in range(m):
-        if map[j][i] == 1 and chk[j][i] == False:
-            chk[j][i] = True
-            cnt += 1
-            maxv = max(maxv, bfs(j,i))
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    graph[x][y] = 0
+    are=1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+
+            if nx<0 or nx>=n or ny<0 or ny >=m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                are+=1
+                queue.append((nx, ny))
+    res.append(are)
+
+for x in range(n):
+    for y in range(m):
+        if graph[x][y] == 1:  # 그림이 그려져있다면 BFS 탐색 시작
+            bfs(x, y)
+            cnt += 1  # 그림 갯수 +1
 
 print(cnt)
-print(maxv)
+print(max(res) if res else 0)  # 가장 큰 영역의 크기 출력 (영역이 없으면 0)
