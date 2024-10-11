dbb71e09d02fc5f8e61f3ce609b2dfaf208b9b08
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-10-11 14:26:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.80 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -4,8 +4,10 @@ def solution(nums):
     pick=len(nums)//2
     kind=len(set(nums))
     
+    # 종류보다 선택 할 포켓몬 수가 적은 경우
     if pick < kind:
         answer=pick
+    # 나머지 경우(종류보다 선택할 포켓몬 수가 더 크거나 같음)
     else:
         answer=kind
     
