bcaa96f2061d6e84f02605b9850b97cc980a4b34
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-10-24 14:55:27 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,13 @@
+def solution(A,B):
+    answer = 0
+
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(len(A)):
+        a=A[i]
+        b=B[i]
+    
+        answer=answer+a*b
+
+    return answer
