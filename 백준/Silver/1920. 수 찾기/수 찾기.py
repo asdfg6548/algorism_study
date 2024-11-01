7d2f6f138ae0b231f794390960ce3a4c3afe0e58
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-11-01 16:32:16 +0900

    [Silver IV] Title: 수 찾기, Time: 184 ms, Memory: 148244 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+
+m=int(input())
+b=list(map(int,input().split()))
+
+a=set(a)
+
+for i in b:
+    if i in a:
+        print(1)
+    else:
+        print(0)
