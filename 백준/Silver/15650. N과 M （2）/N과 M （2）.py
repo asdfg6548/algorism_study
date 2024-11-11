9cd08f1cc541cfca7a5a74fb6ebc71571997e318
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2024-11-11 15:32:03 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,12 @@
+# 조합을 활용
+from itertools import combinations
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+numbers = range(1, n+1)
+
+# permutations를 사용하여 1부터 n까지의 수 중에서 m개를 선택하는 모든 순열을 생성
+for perm in combinations(numbers, m):
+    print(' '.join(map(str, perm)))
