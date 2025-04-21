99bb171e06b04dfd1037c248b1c3d8cc33eda52c
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2025-04-21 11:18:27 +0900

    [Gold V] Title: 수 고르기, Time: 136 ms, Memory: 114036 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,20 +1,21 @@
-n,m=map(int,input().split())
+import sys
+input=sys.stdin.readline
 
+n,m=map(int,input().split())
 arr=[]
 
 for i in range(n):
-    a=int(input())
-    arr.append(a)
+    inp=int(input())
+    arr.append(inp)
 arr.sort()
 
 res=2000000001
 front=0
-back=0
+back=1
 
-while front <= back < n:
+while front<=back<n:
     if arr[back]-arr[front]<m:
         back+=1
-
     else:
         res=min(res,arr[back]-arr[front])
         front+=1
