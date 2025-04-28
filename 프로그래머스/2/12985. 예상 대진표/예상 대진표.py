a0ffbd643e9d7dbcb7ce406ccb3ec1b7892c75f6
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2025-04-28 14:47:19 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,13 +1,15 @@
-from math import ceil
-def solution(n,a,b):
-    answer = 0
+from math import ceil  # 올림 함수 사용
+
+def solution(n, a, b):
+    answer = 0  # 라운드 수를 저장
     
     while True:
-        answer+=1
-        if abs(a-b)==1 and min(a,b) % 2 == 1:
+        answer += 1  # 라운드 증가
+        # A와 B가 인접하고, 작은 번호가 홀수면 대결 조건 만족
+        if abs(a - b) == 1 and min(a, b) % 2 == 1:
             break
-        else:
-            a=ceil(a/2)
-            b=ceil(b/2)
-
-    return answer
+        # 다음 라운드 번호로 갱신: 현재 번호를 2로 나누고 올림
+        a = ceil(a / 2)
+        b = ceil(b / 2)
+    
+    return answer  # A와 B가 만나는 라운드 번호 반환
