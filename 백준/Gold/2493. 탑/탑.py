bfe1a290cfa658acca0afa4110d815ed61cc93b1
seoyougdeok <157785244+asdfg6548@users.noreply.github.com>
2025-05-20 14:55:30 +0900

    [Gold V] Title: 탑, Time: 252 ms, Memory: 207804 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,25 +1,23 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
+
+# tower 값 리스트외에, stack[(탑의 인덱스,탑의 높이)] 을 하나 더 사용
+## 오른쪽에서 왼쪽으로 레이저를 쏘므로 -> 더 높은 탑이 오른쪽에 있으면 왼쪽에 있는 낮은 탑은 레이저를 받을 수 없음 (스택에서 제거)
+# stack에는 레이저를 받을 수 있는 탑들만 보관
 
 n = int(input())
-data = list(map(int,input().split()))
-stack = [(data[0], 1)]
-res = [0 for i in range(n)]
+tower = list(map(int, input().split()))
+stack = []
+result = [0] * n  # 미리 크기 할당
 
-for i in range(1, n):
-    if data[i] < stack[-1][0]:
-        res[i] = stack[-1][1]
-        stack.append((data[i], i + 1))
-    else:
-        while True:
-            stack.pop()
-            if len(stack) == 0:
-                stack.append((data[i], i + 1))
-                break
-            if data[i] < stack[-1][0]:
-                res[i] = stack[-1][1]
-                stack.append((data[i], i + 1))
-                break
+for i in range(n):
+    while stack and stack[-1][1] < tower[i]:  # 현재 탑보다 낮은 탑 제거
+        stack.pop()
+    
+    if stack:  # 스택에 남은 탑이 있으면 수신 가능
+        result[i] = stack[-1][0] + 1  # 1-based 인덱스
+    # else: result[i] = 0 (기본값이 0이므로 생략 가능)
+    
+    stack.append((i, tower[i]))  # 현재 탑 추가
 
-
-print(*res)
+print(" ".join(map(str, result)))
